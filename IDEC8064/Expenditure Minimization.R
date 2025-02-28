e3e6388aa51764c1eeb4ba3f9c0d2e67c06c3085
mpjennings <miles.j2002@gmail.com>
2025-02-28 11:25:32 +1100

    Update Expenditure Minimization.R

--- IDEC8064/Expenditure Minimization.R
+++ IDEC8064/Expenditure Minimization.R
@@ -3,9 +3,9 @@
 A = 1
 a1 = 0.2
 a2 = 0.8
-w1 = 5
-w2 = 4
-U = 52
+w1 = 6
+w2 = 7
+U = 77
   
 xh_1 = (a1/w1) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2)^(1/(a1 + a2))
 xh_2 = (a2/w2) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2)^(1/(a1 + a2))
@@ -17,5 +17,63 @@ e
 
 # Three good Cobb-Douglas
 
+A = 1
+a1 = 0.2
+a2 = 0.8
+a3 = 
+w1 = 6
+w2 = 7
+w3 = 
+U = 77
+
+xh_1 = (a1/w1) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2 * (a3/w3)^-a3)^(1/(a1 + a2 + a3))
 xh_2 = (a2/w2) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2 * (a3/w3)^-a3)^(1/(a1 + a2 + a3))
+xh_3 = (a3/w3) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2 * (a3/w3)^-a3)^(1/(a1 + a2 + a3))
 e = (a1 + a2 + a3) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2 * (a3/w3)^-a3)^(1/(a1 + a2 + a3))
+
+xh_1
+xh_2
+xh_3
+e
+
+# Two good CES
+
+A = 1
+r = 1 - 1/sigma
+sigma = 1.43
+a1 = 0.7
+a2 = 0.3
+w1 = 6
+w2 = 7
+U = 77
+
+xh_1 = U * (a1/w1)^sigma * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma)^(sigma/1-sigma)
+xh_2 = U * (a2/w2)^sigma * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma)^(sigma/1-sigma)
+e = U * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma)^(sigma/1-sigma)
+
+xh_1
+xh_2
+e
+
+# Three good CES
+
+A = 1
+r = 1 - 1/sigma
+sigma =
+a1 = 0.2
+a2 = 0.8
+a3 = 
+w1 = 6
+w2 = 7
+w3 = 
+U = 77
+
+xh_1 = U * (a1/w1)^sigma * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma)^(sigma/1-sigma)
+xh_2 = U * (a2/w2)^sigma * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma)^(sigma/1-sigma)
+xh_3 = U * (a3/w3)^sigma * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma)^(sigma/1-sigma)
+e = U * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma)^(sigma/1-sigma)
+
+xh_1
+xh_2
+xh_3
+e
