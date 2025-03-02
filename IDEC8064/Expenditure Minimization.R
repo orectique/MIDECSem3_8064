162a6d934fab7fdf5e7db86d98be461ff1dc6696
Sreehari Pulickamadhom Sreedhar <orectique@gmail.com>
2025-03-02 18:40:02 +1100

    cv and ev

--- IDEC8064/Expenditure Minimization.R
+++ IDEC8064/Expenditure Minimization.R
@@ -2,80 +2,43 @@
 
 ## Two good Cobb-Douglas
 
-A = 1
-a1 = 0.2
-a2 = 0.8
-w1 = 6
-w2 = 7
-U = 77
+cobb_douglas_two_good_emin <- function(A, a1, a2, w1, w2, U) {
+  xh_1 = (a1/w1) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2)^(1/(a1 + a2))
+  xh_2 = (a2/w2) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2)^(1/(a1 + a2))
+  e = (a1 + a2) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2)^(1/(a1 + a2))
   
-xh_1 = (a1/w1) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2)^(1/(a1 + a2))
-xh_2 = (a2/w2) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2)^(1/(a1 + a2))
-e = (a1 + a2) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2)^(1/(a1 + a2))
-  
-xh_1
-xh_2
-e
+  return(list(xh_1 = xh_1, xh_2 = xh_2, e = e))
+}
 
 ## Three good Cobb-Douglas
 
-A = 1
-a1 = 0.2
-a2 = 0.8
-a3 = 
-w1 = 6
-w2 = 7
-w3 = 
-U = 77
-
-xh_1 = (a1/w1) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2 * (a3/w3)^-a3)^(1/(a1 + a2 + a3))
-xh_2 = (a2/w2) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2 * (a3/w3)^-a3)^(1/(a1 + a2 + a3))
-xh_3 = (a3/w3) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2 * (a3/w3)^-a3)^(1/(a1 + a2 + a3))
-e = (a1 + a2 + a3) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2 * (a3/w3)^-a3)^(1/(a1 + a2 + a3))
-
-xh_1
-xh_2
-xh_3
-e
+cobb_douglas_three_good_emin <- function(A1, a1, a2, a3, w1, w2, w3, U) {
+  xh_1 = (a1/w1) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2 * (a3/w3)^-a3)^(1/(a1 + a2 + a3))
+  xh_2 = (a2/w2) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2 * (a3/w3)^-a3)^(1/(a1 + a2 + a3))
+  xh_3 = (a3/w3) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2 * (a3/w3)^-a3)^(1/(a1 + a2 + a3))
+  e = (a1 + a2 + a3) * (A^-1 * U * (a1/w1)^-a1 * (a2/w2)^-a2 * (a3/w3)^-a3)^(1/(a1 + a2 + a3))
+  
+  return(list(xh_1 = xh_1, xh_2 = xh_2, xh_3 = xh_3, e = e))
+}
 
 ## Two good CES
 
-A = 1
-sigma = 1.09
-r = 1 - 1/sigma
-a1 = 0.2
-a2 = 0.8
-w1 = 5
-w2 = 5
-U = 78
-
-xh_1 = U * (a1/w1)^sigma * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma)^(sigma/(1-sigma))
-xh_2 = U * (a2/w2)^sigma * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma)^(sigma/(1-sigma))
-e = U * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma)^(1/(1 - sigma))
-
-xh_1
-xh_2
-e
+ces_two_good_emin <- function(A, sigma, a1, a2, w1, w2, U) {
+  xh_1 = U * (a1/w1)^sigma * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma)^(sigma/(1-sigma))
+  xh_2 = U * (a2/w2)^sigma * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma)^(sigma/(1-sigma))
+  e = U * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma)^(1/(1 - sigma))
+  
+  return(list(xh_1 = xh_1, xh_2 = xh_2, e = e))
+}
 
 ## Three good CES
 
-A = 1
-sigma = 1.08
-r = 1 - 1/sigma
-a1 = 0.25
-a2 = 0.5
-a3 = 0.25
-w1 = 8
-w2 = 6
-w3 = 8
-U = 66
+ces_three_good_emin <- function(A, sigma, a1, a2, a3, w1, w2, w3, U) {
+  xh_1 = U * (a1/w1)^sigma * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma)^(sigma/(1-sigma))
+  xh_2 = U * (a2/w2)^sigma * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma)^(sigma/(1-sigma))
+  xh_3 = U * (a3/w3)^sigma * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma)^(sigma/(1-sigma))
+  e = U * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma)^(1/(1-sigma))
+  
+  return(list(xh_1 = xh_1, xh_2 = xh_2, xh_3 = xh_3, e = e))
+}
 
-xh_1 = U * (a1/w1)^sigma * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma)^(sigma/(1-sigma))
-xh_2 = U * (a2/w2)^sigma * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma)^(sigma/(1-sigma))
-xh_3 = U * (a3/w3)^sigma * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma)^(sigma/(1-sigma))
-e = U * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma)^(1/(1-sigma))
-
-xh_1
-xh_2
-xh_3
-e
--- IDEC8064/Utility Maximization.R
+++ IDEC8064/Utility Maximization.R
@@ -2,102 +2,42 @@
 
 ## Two good Cobb-Douglas
 
-A = 1
-a1 = 0.75
-a2 = 0.25
-w1 = 6
-w2 = 5
-M = 50
+cobb_douglas_two_good_umax <- function(A, a1, a2, w1, w2, M) {
+  x_1 = (a1/(a1 + a2)) * M/w1
+  x_2 = (a2/(a1 + a2)) * M/w2
+  v = A * a1^a1 * a2^a2 * (a1 + a2)^-(a1 + a2) * w1^(-a1) * w2^(-a2) * M^(a1 + a2)
   
-x_1 = (a1/(a1 + a2)) * M/w1
-x_2 = (a2/(a1 + a2)) * M/w2
-v = A * a1^a1 * a2^a2 * (a1 + a2)^-(a1 + a2) * w1^(-a1) * w2^(-a2) * M^(a1 + a2)
-
-x_1
-x_2 
-v
-
+  return(list(x_1 = x_1, x_2 = x_2, v = v))
+}
+  
 ## Three good Cobb-Douglas
 
-A = 1
-a1=0.75
-a2=0.1
-a3=0.15
-w1=8
-w2=6
-w3=7
-M=70
-
-x_1=a1/(a1+a2+a3)*M/w1
-x_2=a2/(a1+a2+a3)*M/w2
-x_3=a3/(a1+a2+a3)*M/w3
-v=A*(a1/w1)^(a1)*(a2/w2)^(a2)*(a3/w3)^(a3)*(a1+a2+a3)^(-(a1+a2+a3))*M^(a1+a2+a3)
-
-x_1
-x_2
-x_3
-v
+cobb_douglas_three_good_umax <- function(A, a1, a2, a3, w1, w2, w3, M) {
+  x_1=a1/(a1+a2+a3)*M/w1
+  x_2=a2/(a1+a2+a3)*M/w2
+  x_3=a3/(a1+a2+a3)*M/w3
+  v=A*(a1/w1)^(a1)*(a2/w2)^(a2)*(a3/w3)^(a3)*(a1+a2+a3)^(-(a1+a2+a3))*M^(a1+a2+a3)
+  
+  return(list(x_1 = x_1, x_2 = x_2, x_3 = x_3, v = v))
+}
 
 ## Two good CES
 
-A = 1 
-sigma = 0.83
-r = 1 - 1/sigma
-a1 = 0.33
-a2 = 0.67
-w1 = 7
-w2 = 5
-M = 45
-
-x_1 = ( M * (a1/w1)^sigma ) / (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma)
-x_2 = ( M * (a2/w2)^sigma ) / (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma)
-v = A * M * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma) ^ (1/(sigma - 1))
-
-x_1
-x_2
-v
+ces_two_good_umax <- function(A, sigma, a1, a2, w1, w2, M) {
+  x_1 = ( M * (a1/w1)^sigma ) / (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma)
+  x_2 = ( M * (a2/w2)^sigma ) / (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma)
+  v = A * M * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma) ^ (1/(sigma - 1))
+  
+  return(list(x_1 = x_1, x_2 = x_2, v = v))
+}
 
 ## Three good CES
 
-A = 1
-sigma = 0.74
-r = 1 - 1/sigma
-a1 = 0.33
-a2 = 0.4
-a3 = 0.27
-w1 = 5
-w2 = 8
-w3 = 6
-M = 56
-
-x_1 = ( M * (a1/w1)^sigma ) / (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma)
-x_2 = ( M * (a2/w2)^sigma ) / (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma)
-x_3 = ( M * (a3/w3)^sigma ) / (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma)
-v = A * M * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma) ^ (1/(sigma - 1))
-
-x_1
-x_2
-x_3
-v
-
-# Expenditure Minimization
-
-## Two good Cobb-Douglas
-
-A = 1
-a1 = 0.8
-a2 = 0.2
-w1 = 7
-w2 = 7
-U = 78
-
-x_1 = a1 * w1^(-1) * a1^(-a1) *a2^(-a2) * w1^a1 * w2^a2 * U
-x_2 = a2 * w2^(-1) * a1^(-a1) *a2^(-a2) * w1^a1 * w2^a2 * U
-e  = a1^(-a1) *a2^(-a2) * w1^a1 * w2^a2 * U
-
-x_1
-x_2
-e
-
-
-
+ces_three_good_umax <- function(A, sigma, a1, a2, a3, w1, w2, w3, M) {
+  x_1 = ( M * (a1/w1)^sigma ) / (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma)
+  x_2 = ( M * (a2/w2)^sigma ) / (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma)
+  x_3 = ( M * (a3/w3)^sigma ) / (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma)
+  v = A * M * (w1 * (a1/w1)^sigma + w2 * (a2/w2)^sigma + w3 * (a3/w3)^sigma) ^ (1/(sigma - 1))
+  
+  return(list(x_1 = x_1, x_2 = x_2, x_3 = x_3, v = v))
+}
